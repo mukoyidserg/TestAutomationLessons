30761b721447f8d288294f44f4d02a18ebe8fc55
mukoyidserg <mukoyidserg@gmail.com>
2016-09-12 16:12:04 +0300

    Getter and setter methods are set properly.

--- Sergii Mukoid/src/lesson1/part3/companyworker/LogisticsManager.java
+++ Sergii Mukoid/src/lesson1/part3/companyworker/LogisticsManager.java
@@ -3,7 +3,18 @@ public class LogicticsManager extends CompanyWorker {
                                 private String LogArea;
                                 private int Effectiveness;
                                 public String Zone;
-private int Optimize (int NewEff) {Effectiveness = NewEff}
-public String AssZone (String NewZone) {Zone = NewZone}  
+public String getLogArea() {return LogArea;}
+public void LogArea (String LogArea) {
+	this.LogArea = LogArea;
+} 
+public int getEffectiveness() {return Effectiveness;}
+public void setEffectiveness (int Effectiveness) {
+	this.Effectiveness = Effectiveness;
+}
+public String getZone() {return Zone;}
+public void setZone (int Zone) {
+	this.Zone = Zone;
+}
+
 }
 
