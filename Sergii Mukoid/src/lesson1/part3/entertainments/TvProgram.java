52c811c99b8241a7df13a24415ef0046d56e07cd
mukoyidserg <mukoyidserg@gmail.com>
2016-09-12 16:33:33 +0300

    Getter and setter methods are set properly.

--- Sergii Mukoid/src/lesson1/part3/entertainments/TvProgram.java
+++ Sergii Mukoid/src/lesson1/part3/entertainments/TvProgram.java
@@ -2,7 +2,18 @@
 public class TvProgram extends Entertainments {
                                 private String Channel;
                                 private String Genre;
-                                public Boolean ViewModel;
-private void Broadcast() {}
-public boolean ViewModel (boolean NewModel) { ViewModel = NewModel}
+                                private boolean ViewModel;
+public String getChannel() {return Channel;}
+public void setChannel (String Channel) {
+	this.Channel = Channel;
+} 
+public String getGenre() {return Genre;}
+public void setGenre (String Genre) {
+	this.Genre = Genre;
+}
+public boolean getViewModel() {return ViewModel;}
+public void setViewModel (boolean ViewModel) {
+	this.ViewModel = ViewModel;
+}
+
 }
