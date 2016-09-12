7ba378107543dda90d01c1e0d054fdec869fd404
mukoyidserg <mukoyidserg@gmail.com>
2016-09-12 16:17:06 +0300

    Getter and setter methods are set properly.

--- Sergii Mukoid/src/lesson1/part3/companyworker/MarketingManager.java
+++ Sergii Mukoid/src/lesson1/part3/companyworker/MarketingManager.java
@@ -2,8 +2,20 @@
 public class MarketingManager extends CompanyWorker {
                                 private String Sphere;
                                 private int Budget;
-                                public int Kpi;
-private void Event() {}
-public Int AssGoal (Int NewKpi) {Int = NewKpi}  
+                                private int Kpi;
+public String getSphere() {return Sphere;}
+public void Sphere (String Sphere) {
+	this.Sphere = Sphere;
+} 
+public int getBudget() {return Budget;}
+public void setBudget (int Budget) {
+	this.Budget = Budget;
 }
+public int getKpi() {return Kpi;}
+public void setKpi (int Kpi) {
+	this.Kpi = Kpi;
+}
+
+}
+
 
