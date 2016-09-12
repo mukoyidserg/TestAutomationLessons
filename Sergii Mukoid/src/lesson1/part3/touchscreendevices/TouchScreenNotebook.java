5b262ac5907aa781ff2b08d1e483e262d5e00af2
mukoyidserg <mukoyidserg@gmail.com>
2016-09-12 17:32:16 +0300

    Getter and setter methods are set properly.

--- Sergii Mukoid/src/lesson1/part3/touchscreendevices/TouchScreenNotebook.java
+++ Sergii Mukoid/src/lesson1/part3/touchscreendevices/TouchScreenNotebook.java
@@ -3,6 +3,17 @@ public class TouchscreenNotebook extends TouchScreenDevice {
                                 private String VideoCard;
                                 private String PurposeType;
                                 private int NoChargeHours;
-private String ReplaceBat (String NewNoCharge) { NoChargeHours = NewNoCharge }  
-public String IncreaseBat (String IncrNoCharge) { NoChargeHours = IncrNoCharge }  
+public String getVideoCard() {return VideoCard;}
+public void setVideoCard (String VideoCard) {
+	this.VideoCard = VideoCard;
+} 
+public String getPurposeType() {return PurposeType;}
+public void setPurposeType (String PurposeType) {
+	this.PurposeType = PurposeType;
+}
+public int getNoChargeHours() {return NoChargeHours;}
+public void setNoChargeHours (int NoChargeHours) {
+	this.NoChargeHours = NoChargeHours;
+}
+
 }
