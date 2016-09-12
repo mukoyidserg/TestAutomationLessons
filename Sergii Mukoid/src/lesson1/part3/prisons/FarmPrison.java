039ea265fc0ed7fff0d3a6fc5c6d903af307c4f0
mukoyidserg <mukoyidserg@gmail.com>
2016-09-12 16:52:15 +0300

    Getter and setter methods are set properly.

--- Sergii Mukoid/src/lesson1/part3/prisons/FarmPrison.java
+++ Sergii Mukoid/src/lesson1/part3/prisons/FarmPrison.java
@@ -2,7 +2,18 @@
 public class FarmPrison extends Prison {
                                 private String WorkType;
                                 private String ConvLeasing;
-                                public boolean LabourGoal;
-private String SignContr (String NewLease) {ConvLeasing = NewLease}
-public String ChangeDistr (String NewGoal) { LabourGoal = NewGoal}
+                                private boolean LabourGoal;
+public String getWorkType() {return WorkType;}
+public void setWorkType (String WorkType) {
+	this.InstrType = InstrType;
+} 
+public String getConvLeasing() {return ConvLeasing;}
+public void setConvLeasing (String ConvLeasing) {
+	this.ConvLeasing = ConvLeasing;
+}
+public boolean getLabourGoal() {return LabourGoal;}
+public void setLabourGoal (boolean LabourGoal) {
+	this.LabourGoal = LabourGoal;
+}
+
 }
