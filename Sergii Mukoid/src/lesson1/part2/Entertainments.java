e566c1cb41ce3ede5bdecb57ef8edccb2d01fa71
mukoyidserg <mukoyidserg@gmail.com>
2016-09-12 15:10:05 +0300

    Getter and setter methods are set properly.

--- Sergii Mukoid/src/lesson1/part2/Entertainments.java
+++ Sergii Mukoid/src/lesson1/part2/Entertainments.java
@@ -4,22 +4,53 @@ package lesson1.part2;
 public class Entertainments {
                                 private int AvgLength;
                                 private int AvgPrice;
-                                public int AvgInvolved;
-                                public String Media;
+                                private int AvgInvolved;
+                                private String Media;
                                 private int MinAge;
                                 private String Origin;
-                                private String InvolveLevel;
-                                public String Popularity;
+                                private String InvolvementLevel;
+                                private String Popularity;
                                 private String Impact;
                                 private String Prerequisites;
-private void IncreaseSatisf() {}
-private void TriggerEmotions() {}
-private void FinishEvent() {}
-private void AddFacilities() {}
-private int LimitPartic (int LimInvolved) {AvgInvolved = LimInvolved}
-public String Plan () {return Media;}
-public void Engage() {}
-public int ChangePartic (int NewInvolved) {AvgInvolved = NewInvolved}
-public String Advertise (string NewPopularity) {Popularity = NewPopularity}
-public void CreateEvent() {}
+public int getAvgLength() {return AvgLength;}
+public void setAvgLength (int AvgLength) {
+	this.AvgLength = AvgLength;
+}
+public int getAvgPrice() {return AvgPrice;}
+public void setAvgPrice (int AvgPrice) {
+	this.AvgPrice = AvgPrice;
+}
+public int getAvgInvolved() {return AvgInvolved;}
+public void setAvgInvolved (int AvgInvolved) {
+	this.AvgInvolved = AvgInvolved;
+}
+public String getMedia() {return Media;}
+public void setMedia (String Media) {
+	this.Media = Media;
+}
+public int getMinAge() {return MinAge;}
+public void setMinAge (int MinAge) {
+	this.MinAge = MinAge;
+}
+public String getOrigin() {return Origin;}
+public void setOrigin (String Origin) {
+	this.Origin = Origin;
+}
+public String getInvolvementLevel() {return InvolvementLevel;}
+public void setInvolvementLevel (String InvolvementLevel) {
+	this.InvolvementLevel = InvolvementLevel;
+}
+public String getPopularity() {return Popularity;}
+public void setPopularity (String Popularity) {
+	this.Popularity = Popularity;
+}
+public String getImpact() {return Impact;}
+public void setImpact (String Impact) {
+	this.Impact = Impact;
+}
+public String getPrerequisites() {return Prerequisites;}
+public void setPrerequisites (String Prerequisites) {
+	this.Prerequisites = Prerequisites;
+}
+
 }
