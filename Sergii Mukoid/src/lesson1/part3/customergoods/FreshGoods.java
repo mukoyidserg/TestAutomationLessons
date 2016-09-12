4f6bf8f5778a001bb6055a6f7d602841665af6b6
mukoyidserg <mukoyidserg@gmail.com>
2016-09-12 16:24:05 +0300

    Getter and setter methods are set properly.

--- Sergii Mukoid/src/lesson1/part3/customergoods/FreshGoods.java
+++ Sergii Mukoid/src/lesson1/part3/customergoods/FreshGoods.java
@@ -1,8 +1,19 @@
 
 public class FreshGoods extends CustomerGoods {
                                 private boolean GoodsType;
-                                public double FreshnessPerc;
-                                private Boolean DeliveryType;
-private void ChangeMarkCond() {}
-public double ChangeFreshness (double NewPerc) {FreshnessPerc = NewPerc}  
+                                private double FreshnessPercentage;
+                                private boolean DeliveryType;
+public boolean getGoodsType() {return GoodsType;}
+public void setGoodsType (boolean GoodsType) {
+	this.GoodsType = GoodsType;
+} 
+public double getFreshnessPercentage() {return FreshnessPercentage;}
+public void setFreshnessPercentage (double FreshnessPercentage) {
+	this.FreshnessPercentage = FreshnessPercentage;
+}
+public boolean getDeliveryType() {return DeliveryType;}
+public void setDeliveryType (boolean DeliveryType) {
+	this.DeliveryType = DeliveryType;
+}
+
 }
