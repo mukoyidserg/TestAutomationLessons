42695df15707e810b9927365caf40e05afea80fb
mukoyidserg <mukoyidserg@gmail.com>
2016-09-12 17:30:29 +0300

    Getter and setter methods are set properly.

--- Sergii Mukoid/src/lesson1/part3/touchscreendevices/SmartWatch.java
+++ Sergii Mukoid/src/lesson1/part3/touchscreendevices/SmartWatch.java
@@ -3,6 +3,17 @@ public class SmartWatch extends TouchScreenDevice {
                                 private String Connectivity;
                                 private boolean Camera;
                                 private int WaterResist;
-private void Connect()
-public void SetTime()
+public String getConnectivity() {return Connectivity;}
+public void setConnectivity (String Connectivity) {
+	this.Connectivity = Connectivity;
+} 
+public boolean getCamera() {return Camera;}
+public void setCamera (boolean Camera) {
+	this.Camera = Camera;
+}
+public int getWaterResist() {return WaterResist;}
+public void setWaterResist (int WaterResist) {
+	this.WaterResist = WaterResist;
+}
+
 }
