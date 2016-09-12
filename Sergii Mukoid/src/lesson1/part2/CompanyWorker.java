fd4732973b62bf3374e76a11f7393585292ce17f
mukoyidserg <mukoyidserg@gmail.com>
2016-09-12 14:39:26 +0300

    Getter and setter methods are set properly.

--- Sergii Mukoid/src/lesson1/part2/CompanyWorker.java
+++ Sergii Mukoid/src/lesson1/part2/CompanyWorker.java
@@ -5,22 +5,53 @@ public class CompanyWorker {
                                 private int Age;
                                 private int WorkDur;
                                 private int YearsExp;
-                                public int Salary;
-                                public String Position;
+                                private int Salary;
+                                private String Position;
                                 private String Division;
                                 private int Subordinates;
                                 private boolean Relation;
                                 private double Ownership;
                                 private boolean Employment;
-private void Vacation() {}
-private void CompleteTask() {}
-private void LeaveCompany() {}
-private void VisitOffice() {}
-private void CleanDesk() {}
-public void GiveTask() {}
-public void IncreaseMotivation(){}
-public int ChangeSalary (int NewSalary) {Salary = NewSalary}
-public int ChangePosition (int NewPosition) {Position = NewPosition}
-public void Fire() {}
+public int getAge() {return Age;}
+public void setAge (int Age) {
+	this.Age = Age;
+}
+public int getWorkDur() {return WorkDur;}
+public void setWorkDur (int WorkDur) {
+	this.WorkDur = WorkDur;
+}
+public int getYearsExp() {return YearsExp;}
+public void setYearsExp (int YearsExp) {
+	this.YearsExp = YearsExp;
+}
+public int getSalary() {return Salary;}
+public void setSalary (int Salary) {
+	this.Salary = Salary;
+}
+public String getPosition() {return Position;}
+public void setPosition (int Position) {
+	this.Position = Position;
+}
+public String getDivision() {return Division;}
+public void setDivision (String Division) {
+	this.Division = Division;
+}
+public int getSubordinates() {return Subordinates;}
+public void setSubordinates (int Subordinates) {
+	this.Subordinates = Subordinates;
+}
+public boolean getRelation() {return Relation;}
+public void setRelation (int Relation) {
+	this.Relation = Relation;
+}
+public double getOwnership() {return Ownership;}
+public void setOwnership (double Ownership) {
+	this.Ownership = Ownership;
+}
+public boolean getEmployment() {return Employment;}
+public void setEmployment (boolean Employment) {
+	this.Employment = Employment;
+}
+
 }
 
