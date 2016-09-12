0ef00ac0fb2bef8116141379a7816b317da5b8ce
mukoyidserg <mukoyidserg@gmail.com>
2016-09-12 15:50:43 +0300

    Getter and setter methods are set properly.

--- Sergii Mukoid/src/lesson1/part2/TouchScreenDevice.java
+++ Sergii Mukoid/src/lesson1/part2/TouchScreenDevice.java
@@ -3,24 +3,54 @@ package lesson1.part2;
 
 public class TouchScreenDevice {
                                 private int Display;
-                                private String System;
+                                private String OperatingSystem;
                                 private boolean Phoning;
                                 private int Battery;
                                 private int Storage;
-                                private int CpuFreq;
+                                private int CpuFrequency;
                                 private String Color;
                                 private int Weight;
                                 private boolean Cards;
-                                private int DispResolution;
-private void TurnScreen() {}
-private void LoadApps() {}
-private void TurnOff() {}
-private void IndicateBattery() {}
-private void Vibrate() {}
-public void TurnDevice() {}
-public void OpenMenu() {}
-public void OpenApp() {}
-public void ChargeBattery() {}
-public void TurnDataExch() {}
+                                private int DisplayResolution;
+public int getDisplay() {return Display;}
+public void setDisplay (int Display) {
+	this.Display = Display;
+}
+public String getOperatingSystem() {return OperatingSystem;}
+public void setOperatingSystem (String OperatingSystem) {
+	this.OperatingSystem = OperatingSystem;
+}
+public boolean getPhoning() {return Phoning;}
+public void setPhoning (boolean Phoning) {
+	this.Phoning = Phoning;
+}
+public int getBattery() {return Battery;}
+public void setBattery (int Battery) {
+	this.Battery = Battery;
+}
+public int getStorage() {return Storage;}
+public void setDisplay (int Display) {
+	this.Display = Display;
+}
+public int getCpuFrequency() {return CpuFrequency;}
+public void setCpuFrequency (int CpuFrequency) {
+	this.CpuFrequency = CpuFrequency;
+}
+public String getColor() {return Color;}
+public void setColor (String Color) {
+	this.Color = Color;
+}
+public int getWeight() {return Weight;}
+public void setWeight (int Weight) {
+	this.Weight = Weight;
+}
+public boolean getCards() {return Cards;}
+public void setCards (boolean Cards) {
+	this.Cards = Cards;
+}
+public int getDisplayResolution() {return DisplayResolution;}
+public void setDisplayResolution (int DisplayResolution) {
+	this.DisplayResolution = DisplayResolution;
 }
 
+}
