9d6770ca13e19bf6cc098bdf3665470f1a59ab8d
mukoyidserg <mukoyidserg@gmail.com>
2016-09-12 16:28:07 +0300

    Getter and setter methods are set properly.

--- Sergii Mukoid/src/lesson1/part3/entertainments/MusicalConcert.java
+++ Sergii Mukoid/src/lesson1/part3/entertainments/MusicalConcert.java
@@ -3,6 +3,17 @@ public class MusicalConcert extends Entertainments {
                                 private String Genre;
                                 private String Performers;
                                 public String Venue;
-private String NewPerformers (String NewPerf) {Performers = NewPerf}
-public String ChooseVenue (String NewVenue) {Venue = NewVenue}
+public String getGenre() {return Genre;}
+public void setGenre (String Genre) {
+	this.Genre = Genre;
+} 
+public String getPerformers() {return Performers;}
+public void setPerformers (String Performers) {
+	this.Performers = Performers;
+}
+public String getVenue() {return Venue;}
+public void setVenue (String Venue) {
+	this.Venue = Venue;
+}
+
 }
