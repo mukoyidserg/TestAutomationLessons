fd2b7f4a6d9e8beb14fbfda42b72e2cc0bbf0d96
mukoyidserg <mukoyidserg@gmail.com>
2016-09-12 16:28:56 +0300

    Getter and setter methods are set properly.

--- Sergii Mukoid/src/lesson1/part3/entertainments/Party.java
+++ Sergii Mukoid/src/lesson1/part3/entertainments/Party.java
@@ -3,6 +3,17 @@ public class Party extends Entertainments {
                                 private boolean Admission;
                                 public int AlcoVolume;
                                 private String Venue;
-private boolean ChangeAdm (String NewAdm) {Admission = NewAdm}
-public boolean NoAlco (boolean NewVolume) {AlcoVolume = NewVolume}
+public boolean getAdmission() {return Admission;}
+public void setAdmission (boolean Admission) {
+	this.Admission = Admission;
+} 
+public int getAlcoVolume() {return AlcoVolume;}
+public void setAlcoVolume (int AlcoVolume) {
+	this.AlcoVolume = AlcoVolume;
+}
+public String getVenue() {return Venue;}
+public void setVenue (String Venue) {
+	this.Venue = Venue;
+}
+
 }
