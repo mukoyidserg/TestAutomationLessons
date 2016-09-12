2d2ed6995649cad81dcb0dc76512130f2bc6823f
mukoyidserg <mukoyidserg@gmail.com>
2016-09-12 16:49:48 +0300

    Getter and setter methods are set properly.

--- Sergii Mukoid/src/lesson1/part3/musicalinstruments/StringInstruments.java
+++ Sergii Mukoid/src/lesson1/part3/musicalinstruments/StringInstruments.java
@@ -3,6 +3,17 @@ public class StringInstruments extends MusicalInstruments {
                                 private int Strings;
                                 private String StringMaterial;
                                 private String StringsLocation;
-private void StringLoseTune() {}
-public void PluckStrings() {}
+public int getStrings() {return Strings;}
+public void setStrings (int Strings) {
+	this.Strings = Strings;
+} 
+public String getStringMaterial() {return StringMaterial;}
+public void setStringMaterial (String StringMaterial) {
+	this.StringMaterial = StringMaterial;
+}
+public String getStringsLocation() {return StringsLocation;}
+public void setStringsLocation (String StringsLocation) {
+	this.StringsLocation = StringsLocation;
+}
+
 }
