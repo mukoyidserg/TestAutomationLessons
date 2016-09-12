04758f77e9ca8c5c3e063efe7b5fddde12d90d01
mukoyidserg <mukoyidserg@gmail.com>
2016-09-12 17:31:26 +0300

    Getter and setter methods are set properly.

--- Sergii Mukoid/src/lesson1/part3/touchscreendevices/TabletComputer.java
+++ Sergii Mukoid/src/lesson1/part3/touchscreendevices/TabletComputer.java
@@ -1,8 +1,19 @@
 
 public class TabletComputer extends TouchScreenDevice {
-                                public boolean Keyboard;
+                                private boolean Keyboard;
                                 private String SizeClass;
-                                private Boolean Handwriting;
-private void RecognizeHand()
-public String BuyKeyboard (String NewKey) {Keyboard = NewKey}  
+                                private boolean Handwriting;
+public boolean getKeyboard() {return Keyboard;}
+public void setKeyboard (boolean Keyboard) {
+	this.Keyboard = Keyboard;
+} 
+public String getSizeClass() {return SizeClass;}
+public void setSizeClass (String SizeClass) {
+	this.SizeClass = SizeClass;
+}
+public boolean getHandwriting() {return Handwriting;}
+public void setHandwriting (boolean Handwriting) {
+	this.Handwriting = Handwriting;
+}
+
 }
