788be38d32c8684d51221c1db6cd1789ba9a56e2
mukoyidserg <mukoyidserg@gmail.com>
2016-09-12 16:26:26 +0300

    Getter and setter methods are set properly.

--- Sergii Mukoid/src/lesson1/part3/customergoods/NonFoodGoods.java
+++ Sergii Mukoid/src/lesson1/part3/customergoods/NonFoodGoods.java
@@ -3,6 +3,17 @@ public class NonFoodGoods extends CustomerGoods {
                                 public String Precautions;
                                 private String ReturnPolicy;
                                 private String Turnover;
-private String FallSales (String NewTurn) {Turnover = NewTurn}
-public String CheckPrecaut() {return Precautions;}  
+public String getPrecautions() {return Precautions;}
+public void setPrecautions (String Precautions) {
+	this.Precautions = Precautions;
+} 
+public String getReturnPolicy() {return ReturnPolicy;}
+public void setReturnPolicy (String ReturnPolicy) {
+	this.ReturnPolicy = ReturnPolicy;
+}
+public String getTurnover() {return Turnover;}
+public void setTurnover (String Turnover) {
+	this.Turnover = Turnover;
+}
+
 }
