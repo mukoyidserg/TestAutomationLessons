fb029bbf00f9c22e505297e0f59a62db7bc94a6d
mukoyidserg <mukoyidserg@gmail.com>
2016-09-12 15:20:58 +0300

    Getter and setter methods are set properly.

--- Sergii Mukoid/src/lesson1/part2/MusicalInstruments.java
+++ Sergii Mukoid/src/lesson1/part2/MusicalInstruments.java
@@ -2,25 +2,56 @@ package lesson1.part2;
 
 
 public class MusicalInstruments {
-                                private String Class;
+                                private String InstrumentClass;
                                 private String Range;
                                 private String Materials;
                                 private String Type;
                                 private int MaxLoudness;
                                 private int Size;
                                 private int YearMake;
-                                public String Tuning;
+                                private String Tuning;
                                 private String Maker;
                                 private int Weight;
-private void ProduceSound() {}
-private String ChangeSound (string NewRange) {Range = NewRange}
-private String LoseTuning (string LostTuning) {Tuning = LostTuning}
-private String ChangeLoudness (string NewLoudness) {Loudness = NewLoudness}
-private void BecomeDirty() {}
-public String Tune (string NewTuning) {Tuning = NewTuning}
-public void Sound() {}
-public void Clean() {}
-public void Pack() {}
-public void Disassemble() {}
+public String getInstrumentClass() {return InstrumentClass;}
+public void setInstrumentClass (String InstrumentClass) {
+	this.InstrumentClass = InstrumentClass;
+}
+public String getRange() {return Range;}
+public void setRange (String Range) {
+	this.Range = Range;
+}
+public String getMaterials() {return Materials;}
+public void setMaterials (String Materials) {
+	this.Materials = Materials;
+}
+public String getType() {return Type;}
+public void setType (String Type) {
+	this.Type = Type;
+}
+public int getMaxLoudness() {return MaxLoudness;}
+public void setMaxLoudness (String MaxLoudness) {
+	this.MaxLoudness = MaxLoudness;
+}
+public int getSize() {return Size;}
+public void setSize (int Size) {
+	this.Size = Size;
+}
+public int getYearMake() {return YearMake;}
+public void setYearMake (int YearMake) {
+	this.MaxLoudness = MaxLoudness;
+}
+public String getTuning() {return Tuning;}
+public void setTuning (String Tuning) {
+	this.Tuning = Tuning;
+}
+public String getMaker() {return Maker;}
+public void setMaker (String Maker) {
+	this.Maker = Maker;
+}
+public int getWeight() {return Weight;}
+public void setWeight (String Weight) {
+	this.Weight = Weight;
+}
+
 }
 
