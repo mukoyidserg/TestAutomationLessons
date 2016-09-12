d23a81f59bfa026b05c48072fee1c9796124bb22
mukoyidserg <mukoyidserg@gmail.com>
2016-09-12 14:50:53 +0300

    Getter and setter methods are set properly.

--- Sergii Mukoid/src/lesson1/part2/CustomerGoods.java
+++ Sergii Mukoid/src/lesson1/part2/CustomerGoods.java
@@ -3,24 +3,55 @@ package lesson1.part2;
 
 public class CustomerGoods {
                                 private int ShelfLife;
-                                private int ExpDate;
-                                public double Price;
+                                private int ExpiryDate;
+                                private double Price;
                                 private double PackSize;
                                 private boolean Edibility;
-                                private String PackQuant;
-                                public String Ingredients;
+                                private int PackQuantity;
+                                private String Ingredients;
                                 private String Country;
                                 private int MaxTemp;
                                 private double Weight;
-private void Expire() {}
-private void Disintegrate() {}
-public String GetIngredients() {return Ingredients;}
-public void Consume(){}
-public void OpenPack() {}
-public void ClosePack() {}
-public void ThrowAway(){}
-public void Buy() {}
-public int void Return() {}
-public int ChangePrice (int NewPrice) {Price = NewPrice}
+public int getShelfLife() {return ShelfLife;}
+public void setShelfLife (int ShelfLife) {
+	this.ShelfLife = ShelfLife;
+}
+public int getExpiryDate() {return ExpiryDate;}
+public void setExpiryDate (int ExpiryDate) {
+	this.ExpiryDate = ExpiryDate;
+}
+public double getPrice() {return Price;}
+public void setPrice (double Price) {
+	this.Price = Price;
+}
+public double getPackSize() {return PackSize;}
+public void setPackSize (double PackSize) {
+	this.PackSize = PackSize;
+}
+public boolean getEdibility() {return Edibility;}
+public void setEdibility (boolean Edibility) {
+	this.Edibility = Edibility;
+}
+public int getPackQuantity() {return PackQuantity;}
+public void setPackQuantity (int PackQuantity) {
+	this.PackQuantity = PackQuantity;
+}
+public String getIngredients() {return Ingredients;}
+public void setIngredients (String Ingredients) {
+	this.Ingredients = Ingredients;
+}
+public String getCountry() {return Country;}
+public void setCountry (String Country) {
+	this.Country = Country;
+}
+public int getMaxTemp() {return MaxTemp;}
+public void setMaxTemp (int MaxTemp) {
+	this.MaxTemp = MaxTemp;
+}
+public double getWeight() {return Weight;}
+public void setWeight (double Weight) {
+	this.Weight = Weight;
+}
+
 }
 
