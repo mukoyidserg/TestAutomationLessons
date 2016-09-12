12bd6db4df0e9c6503bc51a8d6ecc643fb605a3b
mukoyidserg <mukoyidserg@gmail.com>
2016-09-12 16:46:09 +0300

    Getter and setter methods are set properly.

--- Sergii Mukoid/src/lesson1/part3/musicalinstruments/BrassInstruments.java
+++ Sergii Mukoid/src/lesson1/part3/musicalinstruments/BrassInstruments.java
@@ -3,6 +3,17 @@ public class BrassInstruments extends MusicalInstruments {
                                 private String Family;
                                 private int BoreDiam;
                                 private String BoreForm;
-private void ValveSound() {}
-public void ChangeValves() {}
+public String getFamily() {return Family;}
+public void setFamily (String Family) {
+	this.Family = Family;
+} 
+public int getBoreDiam() {return BoreDiam;}
+public void setBoreDiam (int BoreDiam) {
+	this.BoreDiam = BoreDiam;
+}
+public String getBoreForm() {return BoreForm;}
+public void setBoreForm (String BoreForm) {
+	this.BoreForm = BoreForm;
+}
+
 }
