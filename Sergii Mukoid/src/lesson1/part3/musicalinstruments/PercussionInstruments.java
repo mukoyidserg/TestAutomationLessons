7d33a1648e7196c3b27ba04fa6618e4b8b89b16e
mukoyidserg <mukoyidserg@gmail.com>
2016-09-12 16:48:08 +0300

    Getter and setter methods are set properly.

--- Sergii Mukoid/src/lesson1/part3/musicalinstruments/PercussionInstruments.java
+++ Sergii Mukoid/src/lesson1/part3/musicalinstruments/PercussionInstruments.java
@@ -2,7 +2,18 @@
 public class PercussionInstruments extends MusicalInstruments {
                                 private String SoundMethod;
                                 private String MusicFunction;
-                                private Boolean Spread;
-private void Pitch() {}
-public void ApplObjects() {}
+                                private boolean Spread;
+public String getSoundMethod() {return SoundMethod;}
+public void setSoundMethod (String SoundMethod) {
+	this.SoundMethod = SoundMethod;
+} 
+public String getMusicFunction() {return MusicFunction;}
+public void setMusicFunction (String MusicFunction) {
+	this.MusicFunction = MusicFunction;
+}
+public boolean getSpread() {return Spread;}
+public void setSpread (boolean Spread) {
+	this.Spread = Spread;
+}
+
 }
