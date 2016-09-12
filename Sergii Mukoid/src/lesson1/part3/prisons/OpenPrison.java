f31e77098c2524a85c5d10d0c7b412308ac636ad
mukoyidserg <mukoyidserg@gmail.com>
2016-09-12 16:54:35 +0300

    Getter and setter methods are set properly.

--- Sergii Mukoid/src/lesson1/part3/prisons/OpenPrison.java
+++ Sergii Mukoid/src/lesson1/part3/prisons/OpenPrison.java
@@ -1,8 +1,19 @@
 
 public class OpenPrison extends Prison {
-                                public boolean Employment;
-                                private String RecrFacilities;
-                                private String ReintPrograms;
-private string OpenFacil (string NewFacil) { RecrFacilities = NewFacil}
-public boolean ChngEmploy (boolean NewEmploy) { Employment = NewEmploy }
+                                private boolean Employment;
+                                private String RecreationalFacilities;
+                                private String ReintegrationPrograms;
+public boolean getEmployment() {return Employment;}
+public void setEmployment (boolean Employment) {
+	this.Employment = Employment;
+} 
+public String getRecreationalFacilities() {return RecreationalFacilities;}
+public void setRecreationalFacilities (String RecreationalFacilities) {
+	this.RecreationalFacilities = RecreationalFacilities;
+}
+public String getReintegrationPrograms() {return ReintegrationPrograms;}
+public void setReintegrationPrograms (String ReintegrationPrograms) {
+	this.ReintegrationPrograms = ReintegrationPrograms;
+}
+
 }
