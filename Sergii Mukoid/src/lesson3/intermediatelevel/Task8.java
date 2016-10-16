7c89ec733b4e8e02a56970c6e6ccc7a8d79f14f5
mukoyidserg <mukoyidserg@gmail.com>
2016-10-16 14:39:50 +0300

    Lesson 3 intermediate level completed

--- Sergii Mukoid/src/lesson3/intermediatelevel/Task8.java
+++ Sergii Mukoid/src/lesson3/intermediatelevel/Task8.java
@@ -1,18 +1,17 @@
 package lesson3.intermediatelevel;
 
-import java.util.Arrays;
-
 public class Task8 {
-	
+
 	public void camelCaseSeparator(String camelString) {
 		String wordsInString[] = camelString.split("(?=\\p{Upper})");
-		System.out.println(Arrays.toString(camelString.split("(?=\\p{Upper})")));
+		System.out.print(wordsInString[0].substring(0, 1).toUpperCase() + wordsInString[0].substring(1) + " ");
+		for (int i = 1; i <= wordsInString.length - 1; i++)
+			System.out.print(wordsInString[i].substring(0, 1).toLowerCase() + wordsInString[i].substring(1) + " ");
+
 	}
-	
+
 	public static void main(String[] args) {
 		Task8 myInstance = new Task8();
 		myInstance.camelCaseSeparator("checkChangingProfilePicture");
 	}
-
-
-
+}
