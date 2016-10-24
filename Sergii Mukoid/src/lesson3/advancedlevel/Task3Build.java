f22134cf4dfe11ef17185f3cbe481de6b8702b4d
mukoyidserg <mukoyidserg@gmail.com>
2016-10-25 02:29:51 +0300

    Lesson 3 completed

---
+++ Sergii Mukoid/src/lesson3/advancedlevel/Task3Build.java
@@ -0,0 +1,72 @@
+package lesson3.advancedlevel;
+
+import java.io.File;
+import java.io.IOException;
+
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+public class Task3Build {
+	private int passedTestCases;
+	private int failedTestCases;
+	private int totalBugs;
+	private int highPriorityBugs;
+	private int blockerCriticalBugs;
+
+	public int getPassedTestCases() {
+		return passedTestCases;
+	}
+
+	public int getFailedTestCases() {
+		return failedTestCases;
+	}
+
+	public int getTotalBugs() {
+		return totalBugs;
+	}
+
+	public int getHighPriorityBugs() {
+		return highPriorityBugs;
+	}
+
+	public int getBlockerCriticalBugs() {
+		return blockerCriticalBugs;
+	}
+
+	public void readTestResults(String inputFile) throws BiffException, IOException {
+		Sheet sheetWithInput = Workbook.getWorkbook(new File(inputFile)).getSheet(0);
+		this.passedTestCases = Integer.parseInt(sheetWithInput.getCell(1, 0).getContents());
+		this.failedTestCases = Integer.parseInt(sheetWithInput.getCell(1, 1).getContents());
+		this.totalBugs = Integer.parseInt(sheetWithInput.getCell(1, 2).getContents());
+		this.highPriorityBugs = Integer.parseInt(sheetWithInput.getCell(1, 3).getContents());
+		this.blockerCriticalBugs = Integer.parseInt(sheetWithInput.getCell(1, 4).getContents());
+	}
+
+	public void buildAssesment(int passedTestCases, int failedTestCases, int totalBugs, int highPriorityBugs,
+			int blockerCriticalBugs) {
+		Task3Metrics metrics = new Task3Metrics(passedTestCases, failedTestCases, totalBugs, highPriorityBugs,
+				blockerCriticalBugs);
+		if (metrics.isBuildSuccess() == true)
+			System.out.println("--Build Assesment--\nCurrent build is stable!");
+		else {
+			System.out.println("--Build Assesment--\nCurrent build cannot be released!\nThe reasons are:");
+			if (metrics.isTestCasesPercentageSuccess() == false)
+				System.out.println("More than 20 percent test cases failed");
+			if (metrics.isHighPriorityBugsSuccess() == false)
+				System.out.println("More than 10 unsolved high priority bugs");
+			if (metrics.isBlockerSevereBugsSuccess() == false)
+				System.out.println("At least 5 unsolved Blocker/Critical bugs");
+			if (metrics.isBlockerBugsPercentageSuccess() == false)
+				System.out.println("More than 5 percent of unsolved nugs are critical");
+		}
+	}
+
+	public static void main(String[] args) throws BiffException, IOException {
+		String workFile = "C://Users//SERG//Desktop//UserInput.xls";
+		Task3Build myInstance = new Task3Build();
+		myInstance.readTestResults(workFile);
+		myInstance.buildAssesment(myInstance.getPassedTestCases(), myInstance.getFailedTestCases(),
+				myInstance.getTotalBugs(), myInstance.getHighPriorityBugs(), myInstance.getBlockerCriticalBugs());
+	}
+}
---
+++ Sergii Mukoid/src/lesson3/advancedlevel/Task3Metrics.java
@@ -0,0 +1,47 @@
+package lesson3.advancedlevel;
+
+public class Task3Metrics {
+	private boolean testCasesPercentageSuccess = true;
+	private boolean highPriorityBugsSuccess = true;
+	private boolean blockerSevereBugsSuccess = true;
+	private boolean blockerBugsPercentageSuccess = true;
+	private boolean buildSuccess = false;
+
+	public boolean isTestCasesPercentageSuccess() {
+		return testCasesPercentageSuccess;
+	}
+
+	public boolean isHighPriorityBugsSuccess() {
+		return highPriorityBugsSuccess;
+	}
+
+	public boolean isBlockerSevereBugsSuccess() {
+		return blockerSevereBugsSuccess;
+	}
+
+	public boolean isBlockerBugsPercentageSuccess() {
+		return blockerBugsPercentageSuccess;
+	}
+
+	public boolean isBuildSuccess() {
+		return buildSuccess;
+	}
+
+	public Task3Metrics(int passedTestCases, int failedTestCases, int totalBugs, int highPriorityBugs,
+			int blockerCriticalBugs) {
+		int totalTestCases = passedTestCases + failedTestCases;
+		if ((float) failedTestCases / (float) totalTestCases >= 0.2)
+			this.testCasesPercentageSuccess = false;
+		if (highPriorityBugs > 10)
+			this.highPriorityBugsSuccess = false;
+		if (blockerCriticalBugs >= 5)
+			this.blockerSevereBugsSuccess = false;
+		if ((float) blockerCriticalBugs / (float) totalBugs > 0.05)
+			this.blockerBugsPercentageSuccess = false;
+		if (testCasesPercentageSuccess == highPriorityBugsSuccess == blockerSevereBugsSuccess == blockerBugsPercentageSuccess == true)
+			this.buildSuccess = true;
+		System.out.println("--Build Statistics--\nPassed Test Cases: " + passedTestCases + "\nFailed Test Cases: "
+				+ failedTestCases + "\nTotal Bugs: " + totalBugs + "\nHigh Priority Bugs: " + highPriorityBugs
+				+ "\nBlocker and Critical Bugs: " + blockerCriticalBugs);
+	}
+}
